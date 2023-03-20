a0597ae7ef3f7473d233a38b53b816769052ef92
(Valentin Styza) <styzavalentin@gmail.com>
2023-03-20 16:18:38 +0100

    model booking creation

---
+++ db/migrate/20230320151127_create_bookings.rb
@@ -0,0 +1,14 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :friend, null: false, foreign_key: true
+      t.integer :total_price
+      t.string :status
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
