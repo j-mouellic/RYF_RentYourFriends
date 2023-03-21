e04d3ffcfcc11c44189e55153473b226219c94b2
julien mouellic <ju.mouellic@gmail.com>
2023-03-21 17:17:13 +0100

    devise sign out et css

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :booking
   belongs_to :user
+  AUTHORIZED_RATINGS = (1..5)
 
+  validates :content, presence: true
+  validates :review_rating, inclusion: { in: AUTHORIZED_RATINGS }
   validates :user_id, uniqueness: true
   validates :booking_id, uniqueness: true
 end
