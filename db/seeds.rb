061f02e79ac4725d43696edfc824ce9b50ea62a6
julien mouellic <ju.mouellic@gmail.com>
2023-03-21 12:00:55 +0100

    Seed V1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+Friend.destroy_all
+
+puts "Creating 20 friends ..."
+20.times do
+  friend = Friend.new(
+    name: Faker::Name.name,
+    date: Faker::Date.birthday(min_age: 18, max_age: 65),
+    price: Faker::Number.between(from: 100, to: 1000),
+    genre: Faker::Gender.type,
+    address: Faker::Address.full_address,
+    picture: Faker:: ,
+    activity_wedding: [true, false].sample,
+    activity_sam: [true, false].sample,
+    activity_diner: [true, false].sample,
+    activity_birthday: [true, false].sample,
+    activity_bowling: [true, false].sample,
+    activity_hike: [true, false].sample,
+    activity_movie: [true, false].sample,
+    activity_picnic: [true, false].sample,
+    activity_concert: [true, false].sample,
+    activity_karaoke: [true, false].sample,
+    activity_shopping: [true, false].sample,
+    activity_sport: [true, false].sample,
+    activity_barbecue: [true, false].sample,
+    activity_camping: [true, false].sample,
+    activity_paintball: [true, false].sample,
+    default_complotist: [true, false].sample,
+    default_impatient: [true, false].sample,
+    default_disorganized: [true, false].sample,
+    default_cheap: [true, false].sample,
+    default_arrogant: [true, false].sample,
+    quality_funny: [true, false].sample,
+    quality_reliable: [true, false].sample,
+    quality_honest: [true, false].sample,
+    quality_good_looking: [true, false].sample,
+    quality_brave: [true, false].sample
+  )
+  friend.save!
+end
+puts " Finished!"
