588bda63c26ba671e13606d43e638d7114efc017
(Valentin Styza) <styzavalentin@gmail.com>
2023-03-22 15:05:45 +0100

    price calculating

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,6 +4,6 @@ import flatpickr from "flatpickr"
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
-  console.log("coucou")
   flatpickr(this.element)
-}}
+}
+}
