e3d65bff7ec999e9556a1929445aea1b0570ee17
(Valentin Styza) <styzavalentin@gmail.com>
2023-03-22 11:06:55 +0100

    description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_170149) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_095721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_170149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.text "description"
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
