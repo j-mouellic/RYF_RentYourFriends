ad57e2bb2a2500285fdaae8081153139107b91a1
julien mouellic <ju.mouellic@gmail.com>
2023-03-22 13:59:57 +0100

    account user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @bookings = Booking.where(user_id: current_user.id)
+    @friends = @user.friends
   end
 end
