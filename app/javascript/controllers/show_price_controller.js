2baf4f6b1966d804b9ecb3361ae92c3214478f58
(Valentin Styza) <styzavalentin@gmail.com>
2023-03-24 11:58:25 +0100

    seed and popup

--- app/javascript/controllers/show_price_controller.js
+++ app/javascript/controllers/show_price_controller.js
@@ -33,11 +33,4 @@ export default class extends Controller {
     resultPrice.innerText = `${PrixRemise} €`;
   }
 
-tada(event) {
-  Swal.fire({
-    imageUrl: 'https://placeholder.pics/svg/300x1500',
-    imageHeight: 1500,
-    imageAlt: 'A tall image'
-  })
-}
 }
